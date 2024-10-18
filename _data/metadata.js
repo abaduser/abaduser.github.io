fadfe2efeab92341af645461aea9c7fb63aa64fa
abaduser <abaduser.pw@proton.me>
2024-10-17 18:34:30 -0700

    11nty example starter

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Abaduser's Blog",
+	url: "https://abaduser.pw/",
+	language: "en",
+	description: "I am writing about my experiences as a computer person.",
+	author: {
+		name: "abaduser",
+		email: "abaduser.pw@proton.me",
+		url: "https://abaduser.pw/aboutme/",
+	}
+}
